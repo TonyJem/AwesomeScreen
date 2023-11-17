9fd9c5a03349077eb42b1bfc726451c5bc13ee9f
TonyJem <a.jemanov@gmail.com>
2023-11-17 21:48:53 +0200

    Update AwesomeEmptyView.States.swift

--- AwesomeScreen/DesignSystem/Components/AwesomeEmptyView.States.swift
+++ AwesomeScreen/DesignSystem/Components/AwesomeEmptyView.States.swift
@@ -1,4 +1,5 @@
 import Foundation
+import UIKit
 
 extension AwesomeEmptyView {
 
@@ -6,6 +7,29 @@ extension AwesomeEmptyView {
 
         case emptyControlUnits
 
+    }
+
+}
+
+extension AwesomeEmptyView.State {
+
+    func createViewState() -> AwesomeEmptyView.ViewState {
+        var title: String
+        var subtitle: String
+        var image: UIImage
+
+        switch self {
+        case .emptyControlUnits:
+            image = UIImage()
+            title = "title"
+            subtitle = "subtitle"
+        }
+        
+        return .init(
+            title: title,
+            subtitle: subtitle,
+            image: image
+        )
     }
 
 }
