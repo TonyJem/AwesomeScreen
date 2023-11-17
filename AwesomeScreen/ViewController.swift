ecfca72b23ccccd167465a46249c0ed14deaf5e6
TonyJem <a.jemanov@gmail.com>
2023-11-17 19:14:00 +0200

    Initial Commit

---
+++ AwesomeScreen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AwesomeScreen
+//
+//  Created by Office-iMac on 2023-11-17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
