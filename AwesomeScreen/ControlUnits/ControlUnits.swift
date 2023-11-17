da71249b83e275e9bf1957e294c9452abaeea48b
TonyJem <a.jemanov@gmail.com>
2023-11-17 21:58:28 +0200

    Minor typo correction

--- AwesomeScreen/ControlUnits/ControlUnits.swift
+++ AwesomeScreen/ControlUnits/ControlUnits.swift
@@ -3,6 +3,6 @@ import Foundation
 /// `ControlUnits` is an empty struct intended to delimit the namespace.
 struct ControlUnits {
     private init() {
-        // Intentiaonnally unimplemened. This struct is needed to delimit the namespace only.
+        // Intentionally unimplemented. This struct is needed to delimit the namespace only.
     }
 }
