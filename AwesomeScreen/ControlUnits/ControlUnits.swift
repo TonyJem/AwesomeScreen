e8abd6d69050fcc5b91697350eaf805d1b0622c8
TonyJem <a.jemanov@gmail.com>
2023-11-17 20:30:57 +0200

    Start creating VIPER module file structure

---
+++ AwesomeScreen/ControlUnits/ControlUnits.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+/// `ControlUnits` is an empty struct intended to delimit the namespace.
+struct ControlUnits {
+    private init() {
+        // Intentiaonnally unimplemened. This struct is needed to delimit the namespace only.
+    }
+}
--- AwesomeScreen/SceneDelegate.swift
+++ AwesomeScreen/SceneDelegate.swift
@@ -8,7 +8,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
         let navigationController = UINavigationController()
-        let viewController = ViewController()
+        let viewController = ControlUnitsViewController()
         navigationController.viewControllers = [viewController]
         window.rootViewController = navigationController
         window.makeKeyAndVisible()
