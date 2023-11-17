1feb4a8ca51eebc94d999850ec3cf77ad4e1a4e2
TonyJem <a.jemanov@gmail.com>
2023-11-17 21:35:25 +0200

    Create AwesomeEmptyView.ViewState

---
+++ AwesomeScreen/DesignSystem/Components/AwesomeEmptyView.States.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+extension AwesomeEmptyView {
+
+    enum State {
+
+        case emptyControlUnits
+
+    }
+
+}
--- AwesomeScreen/DesignSystem/Components/AwesomeEmptyView.swift
+++ AwesomeScreen/DesignSystem/Components/AwesomeEmptyView.swift
@@ -1,6 +1,7 @@
 import SwiftUI
 
 struct AwesomeEmptyView: View {
+
     var body: some View {
         ZStack {
             Color.red
@@ -11,4 +12,33 @@ struct AwesomeEmptyView: View {
             .padding()
         }
     }
+
+}
+
+extension AwesomeEmptyView {
+
+    final class ViewState {
+
+        var title: String
+        var subtitle: String
+        var image: UIImage
+
+        init(
+            title: String,
+            subtitle: String,
+            image: UIImage
+        ) {
+            self.title = title
+            self.subtitle = subtitle
+            self.image = image
+        }
+
+        static let intial: ViewState = ViewState(
+            title: "",
+            subtitle: "",
+            image: UIImage()
+        )
+
+    }
+
 }
