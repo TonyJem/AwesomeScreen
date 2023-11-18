10f73e7906053571bc656e22460b4d1bc644e369
TonyJem <a.jemanov@gmail.com>
2023-11-18 15:49:20 +0200

    Update AwesomeFailureView from mock to reality

--- AwesomeScreen/DesignSystem/Components/AwesomeFailureView/AwesomeFailureView.swift
+++ AwesomeScreen/DesignSystem/Components/AwesomeFailureView/AwesomeFailureView.swift
@@ -3,10 +3,39 @@ import SwiftUI
 struct AwesomeFailureView: View {
 
     var body: some View {
+        VStack(spacing: .spacingM) {
 
-        Text("Hello! I'm AwesomeFailureView!")
-            .foregroundColor(.yellow)
+            VStack(spacing: .spacingXXS) {
+                titleText()
+                subtitleText()
+            }
 
+            Button("Try Again") {
+                print("🟢 didTap Action Button in AwesomeFailureView")
+            }
+            .padding(.vertical, .spacingXS)
+
+        }
+        .padding(.horizontal, .spacingXL)
     }
 
+    private func titleText() -> some View {
+        Text("Unable to Load Control Unit List")
+            .fontWithLineHeight(
+                font: .Branded.title,
+                lineHeight: 24
+            )
+            .foregroundColor(Color(.Branded.white))
+            .multilineTextAlignment(.center)
+    }
+
+    private func subtitleText() -> some View {
+        Text("Please try again later.")
+            .fontWithLineHeight(
+                font: .Branded.subtitle,
+                lineHeight: 16
+            )
+            .foregroundColor(Color(.Branded.white))
+            .multilineTextAlignment(.center)
+    }
 }
