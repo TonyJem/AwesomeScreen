c165b0a98687654ade116f5dbb9c3f003ad91a3d
TonyJem <a.jemanov@gmail.com>
2023-11-18 09:19:17 +0200

    Turn on light mode for status bar for all App

--- AwesomeScreen/Features/ControlUnits/View/ControlUnits.ViewController.swift
+++ AwesomeScreen/Features/ControlUnits/View/ControlUnits.ViewController.swift
@@ -14,6 +14,12 @@ class ControlUnitsViewController: UIViewController {
 
     private func setupView() {
 
+        // TODO: Need to dcide if we want to keep it here or move ot more relevant palce
+        navigationController?.navigationBar.titleTextAttributes = [
+            NSAttributedString.Key.foregroundColor: UIColor.white
+        ]
+
+        // TODO: Move plain text to screen viewState and also use localized
         title = "Control Units"
 
         let contentView = ControlUnits.ContentView()
---
+++ AwesomeScreen/Features/ViewController.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationController?.navigationBar.titleTextAttributes = [ NSAttributedString.Key.foregroundColor : UIColor.white ]
+
+        view.backgroundColor = .green
+
+        title = "Control Units"
+
+
+    }
+
+}
