de7aa7779c246f86a5c18970096dd8791b904c12
TonyJem <a.jemanov@gmail.com>
2023-11-19 05:56:03 +0200

    Add ControlUnits.ListView.ViewState

--- AwesomeScreen/Features/ControlUnits/View/SubViews/ControlUnits.ListView.swift
+++ AwesomeScreen/Features/ControlUnits/View/SubViews/ControlUnits.ListView.swift
@@ -24,3 +24,19 @@ extension ControlUnits {
     }
 
 }
+
+extension ControlUnits.ListView {
+
+    // TODO: Perhaps is needed to make ViewState: Equatable <- we'll see when be setting SwiftUI bindings
+    struct ViewState {
+
+        let listItemViewStates: [ControlUnits.ListView.ItemView.ViewState]
+
+        init(listItemViewStates: [ControlUnits.ListView.ItemView.ViewState]
+        ) {
+            self.listItemViewStates = listItemViewStates
+        }
+
+    }
+
+}
