f9e953947eea97d88bcce82e2ebf418d27a0b13c
TonyJem <a.jemanov@gmail.com>
2023-11-19 18:25:01 +0200

    Move mock data from Presener into Interactor

--- AwesomeScreen/Features/ControlUnits/Interactor/ControlUnits.Interactor.swift
+++ AwesomeScreen/Features/ControlUnits/Interactor/ControlUnits.Interactor.swift
@@ -13,11 +13,62 @@ extension ControlUnits {
         // MARK: - Public
 
         func getControlUnitsViewStates() -> [ControlUnits.ListView.ItemView.ViewState] {
-            print("🟢 getControlUnitsViewStates in Interactor")
-            return []
+            return createMockViewStates()
         }
 
         // MARK: - Private
+
+        private func createMockViewStates() -> [ControlUnits.ListView.ItemView.ViewState] {
+            let itemViewState00 = ControlUnits.ListView.ItemView.ViewState(
+                id: "ID 12",
+                title: "Engine",
+                image: .awesomeImage(.testImage00),
+                badge: .faulty,
+                action: { print("🟡 didTap on item00") }
+            )
+
+            let itemViewState01 = ControlUnits.ListView.ItemView.ViewState(
+                id: "ID 06",
+                title: "Surround View Camera",
+                image: .awesomeImage(.testImage01),
+                badge: nil,
+                action: { print("🟡🟡 didTap on item01") }
+            )
+
+            let itemViewState02 = ControlUnits.ListView.ItemView.ViewState(
+                id: "ID 08",
+                title: "Rear Short Range Radar Right",
+                image: .awesomeImage(.testImage02),
+                badge: nil,
+                action: { print("🟡🟡🟡 didTap on item02") }
+            )
+
+            let itemViewState03 = ControlUnits.ListView.ItemView.ViewState(
+                id: "ID 01",
+                title: "Airbag",
+                image: .awesomeImage(.testImage03),
+                badge: nil,
+                action: { print("🟡🟡🟡🟡 didTap on item03") }
+            )
+
+            let itemViewState04 = ControlUnits.ListView.ItemView.ViewState(
+                id: "ID 43",
+                title: "Front Left Light Element",
+                image: .awesomeImage(.testImage04),
+                badge: .notReachable,
+                action: { print("🟡🟡🟡🟡🟡 didTap on item04") }
+            )
+
+            let listItemViewStates: [ControlUnits.ListView.ItemView.ViewState] = [
+                itemViewState00,
+                itemViewState01,
+                itemViewState02,
+                itemViewState03,
+                itemViewState04
+            ]
+
+            return listItemViewStates
+        }
 
     }
 
--- AwesomeScreen/Features/ControlUnits/Presenter/ControlUnits.Presenter.swift
+++ AwesomeScreen/Features/ControlUnits/Presenter/ControlUnits.Presenter.swift
@@ -37,57 +37,8 @@ extension ControlUnits {
         }
 
         private func showAvailableControlUnits() {
-
-            let itemViewState00 = ControlUnits.ListView.ItemView.ViewState(
-                id: "ID 12",
-                title: "Engine",
-                image: .awesomeImage(.testImage00),
-                badge: .faulty,
-                action: { print("🟡 didTap on item00") }
-            )
-
-            let itemViewState01 = ControlUnits.ListView.ItemView.ViewState(
-                id: "ID 06",
-                title: "Surround View Camera",
-                image: .awesomeImage(.testImage01),
-                badge: nil,
-                action: { print("🟡🟡 didTap on item01") }
-            )
-
-            let itemViewState02 = ControlUnits.ListView.ItemView.ViewState(
-                id: "ID 08",
-                title: "Rear Short Range Radar Right",
-                image: .awesomeImage(.testImage02),
-                badge: nil,
-                action: { print("🟡🟡🟡 didTap on item02") }
-            )
-
-            let itemViewState03 = ControlUnits.ListView.ItemView.ViewState(
-                id: "ID 01",
-                title: "Airbag",
-                image: .awesomeImage(.testImage03),
-                badge: nil,
-                action: { print("🟡🟡🟡🟡 didTap on item03") }
-            )
-
-            let itemViewState04 = ControlUnits.ListView.ItemView.ViewState(
-                id: "ID 43",
-                title: "Front Left Light Element",
-                image: .awesomeImage(.testImage04),
-                badge: .notReachable,
-                action: { print("🟡🟡🟡🟡🟡 didTap on item04") }
-            )
-
-            let listItemViewStates: [ControlUnits.ListView.ItemView.ViewState] = [
-                itemViewState00,
-                itemViewState01,
-                itemViewState02,
-                itemViewState03,
-                itemViewState04
-            ]
-
+            let listItemViewStates = interactor.getControlUnitsViewStates()
             let listViewState = ControlUnits.ListView.ViewState(listItemViewStates: listItemViewStates)
-
             viewState = .unitsAvailable(listViewState)
         }
 
