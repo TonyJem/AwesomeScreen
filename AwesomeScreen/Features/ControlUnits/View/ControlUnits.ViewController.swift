1bc200a00cb46bda49286164e474478cd24b3324
TonyJem <a.jemanov@gmail.com>
2023-11-25 11:41:30 +0200

    004: Refactor ControlUnits.ViewController

--- AwesomeScreen/Features/ControlUnits/View/ControlUnits.ViewController.swift
+++ AwesomeScreen/Features/ControlUnits/View/ControlUnits.ViewController.swift
@@ -34,7 +34,6 @@ extension ControlUnits {
             super.viewDidLoad()
 
             searchController.searchBar.delegate = self
-
             setupView()
         }
 
@@ -42,112 +41,109 @@ extension ControlUnits {
 
         private func setupView() {
 
-            // TODO: Need to decide if we want to keep it here or move ot more relevant palce
-            navigationController?.navigationBar.titleTextAttributes = [
-                NSAttributedString.Key.foregroundColor: UIColor(cgColor: .Branded.foregroundPrimary)
-            ]
-
-            title = L10n.ControlUnits.screenTitle
-
-            searchControllerSetup()
-
-            // TODO: Show searchButton only on main screen
-            let updateButton = updateButton()
-
-            navigationItem.rightBarButtonItems = [
-                updateButton
-            ]
-
-            let image = UIImage.awesomeSymbol(.magnifyingGlass).withTintColor(
-                .white,
-                renderingMode: .alwaysOriginal
-            )
-            UISearchBar.appearance().setImage(image, for: .search, state: .normal)
+            setupNavigationBar()
 
             // TODO: Think we have to set backgound color here, instead of settgin it in Host
-
             let contentView = ControlUnits.ContentView(presenter: presenter)
             let host = ViewHostingController(contentView)
 
             host.add(to: view, useSafeArea: true)
         }
 
-        // TODO: Think if all actions shouldInformPreseneter
-        @objc private func didTapUpdateButton() {
-            presenter.reloadControlUnits()
+        private func setupNavigationBar() {
+            setupTitle()
+            setupNavigationBarButtons()
+            setupSearchController()
         }
 
-        @objc private func didTapSearchButton() {
-            // TODO: Put focus on search bar immediatelly, when keyboard opens
-            navigationItem.searchController = searchController
+        private func setupTitle() {
+            navigationController?.navigationBar.titleTextAttributes = [
+                NSAttributedString.Key.foregroundColor: UIColor(cgColor: .Branded.foregroundPrimary)
+            ]
+            title = L10n.ControlUnits.screenTitle
         }
 
-        private func updateButton() -> UIBarButtonItem {
-            let updateButton = UIBarButtonItem(
-                image: .awesomeSymbol(.arrowClockwiseIcloud),
-                style: .plain,
-                target: self,
-                action: #selector(didTapUpdateButton)
-            )
-            updateButton.tintColor = .Branded.foregroundHighlight
-            return updateButton
+        private func setupNavigationBarButtons() {
+            let updateButton = updateButton()
+            navigationItem.rightBarButtonItems = [
+                updateButton
+            ]
         }
 
-        private func searchButton() -> UIBarButtonItem {
-            let searchButton = UIBarButtonItem(
-                image: .awesomeSymbol(.magnifyingGlass),
-                style: .plain,
-                target: self,
-                action: #selector(didTapSearchButton)
-            )
-            searchButton.tintColor = .Branded.foregroundPrimary
-            return searchButton
-        }
+    }
 
-        // TODO: Need to setup text color an check Search field UI in general
-        private func searchControllerSetup() {
-            searchController.searchResultsUpdater = self
-            searchController.obscuresBackgroundDuringPresentation = false
-            searchController.searchBar.placeholder = L10n.ControlUnits.searchBarPlaceholder
-            definesPresentationContext = true
+}
 
-            let textFieldInsideSearchBar = searchController.searchBar.value(forKey: "searchField") as? UITextField
+// MARK: - Setup NavigationBar Buttons
 
-            textFieldInsideSearchBar?.textColor = .Branded.foregroundPrimary
+extension ControlUnits.ViewController {
 
-            // TODO: Redo force unwrap
-            let textFieldInsideSearchBarLabel = textFieldInsideSearchBar!.value(forKey: "placeholderLabel") as? UILabel
-            textFieldInsideSearchBarLabel?.textColor = .Branded.foregroundSecondary
+    private func searchButton() -> UIBarButtonItem {
+        let searchButton = UIBarButtonItem(
+            image: .awesomeSymbol(.magnifyingGlass),
+            style: .plain,
+            target: self,
+            action: #selector(didTapSearchButton)
+        )
+        searchButton.tintColor = .Branded.foregroundPrimary
+        return searchButton
+    }
 
-            // TODO: Try Add Branded color
-            let cancelButtonAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
-            UIBarButtonItem
-                .appearance()
-                .setTitleTextAttributes(cancelButtonAttributes, for: .normal)
-        }
+    // TODO: didTapSearchButton should be handled in Presenter
+    // TODO: Put focus on search bar immediatelly, when keyboard opens
+    @objc private func didTapSearchButton() {
+        navigationItem.searchController = searchController
+    }
 
-        private func showSearchButton() {
-            let searchButton = searchButton()
-            // enables seamless "simple solution" mentioned in suggestion: #01
-            guard let items = navigationItem.rightBarButtonItems,
-                  items.count < 2 else {
-                return
-            }
-            navigationItem.rightBarButtonItems?.append(searchButton)
-        }
+    private func updateButton() -> UIBarButtonItem {
+        let updateButton = UIBarButtonItem(
+            image: .awesomeSymbol(.arrowClockwiseIcloud),
+            style: .plain,
+            target: self,
+            action: #selector(didTapUpdateButton)
+        )
+        updateButton.tintColor = .Branded.foregroundHighlight
+        return updateButton
+    }
 
-        private func hideSearchButton() {
-            // Improvement suggestion: #01
-            // Here is a simple solution, made just for now, which looks to be working for current layout and
-            // which could be broken if more buttons in navBar will be needed to add in the future...
-            // In real case it would be a suggestion for Designer to move "Search" button to the "unitsAvailable" screen's contentView
-            // and avoid placing it in NavBar.
-            // With such improvement it could appear together with content view, and that will improve general UX impresion to the User.
-            if navigationItem.rightBarButtonItems?.count == 2 {
-                navigationItem.rightBarButtonItems?.removeLast()
-            }
-        }
+    @objc private func didTapUpdateButton() {
+        presenter.reloadControlUnits()
+    }
 
+}
+
+// MARK: - Setup SearchController
+extension ControlUnits.ViewController {
+
+    // TODO: Need to setup text color an check Search field UI in general
+    private func setupSearchController() {
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = L10n.ControlUnits.searchBarPlaceholder
+        definesPresentationContext = true
+
+        setupSearchImage()
+
+        let textFieldInsideSearchBar = searchController.searchBar.value(forKey: "searchField") as? UITextField
+        textFieldInsideSearchBar?.textColor = .Branded.foregroundPrimary
+
+        // TODO: Redo force unwrap
+        let textFieldInsideSearchBarLabel = textFieldInsideSearchBar!.value(forKey: "placeholderLabel") as? UILabel
+        textFieldInsideSearchBarLabel?.textColor = .Branded.foregroundSecondary
+
+        // TODO: Try Add Branded color
+        let cancelButtonAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        UIBarButtonItem
+            .appearance()
+            .setTitleTextAttributes(cancelButtonAttributes, for: .normal)
+    }
+
+    private func setupSearchImage() {
+        let image = UIImage.awesomeSymbol(.magnifyingGlass).withTintColor(
+            .white,
+            renderingMode: .alwaysOriginal
+        )
+        UISearchBar.appearance().setImage(image, for: .search, state: .normal)
     }
 
 }
@@ -156,6 +152,7 @@ extension ControlUnits {
 
 extension ControlUnits.ViewController: ControlUnitsViewProtocol {
 
+    // MARK: Public
     func render(viewState: ControlUnits.ViewState) {
         switch viewState {
         case
@@ -167,6 +164,29 @@ extension ControlUnits.ViewController: ControlUnitsViewProtocol {
         case
                 .unitsAvailable:
             showSearchButton()
+        }
+    }
+
+    // MARK: Private
+    private func showSearchButton() {
+        let searchButton = searchButton()
+        // enables seamless "simple solution" mentioned in suggestion: #01
+        guard let items = navigationItem.rightBarButtonItems,
+              items.count < 2 else {
+            return
+        }
+        navigationItem.rightBarButtonItems?.append(searchButton)
+    }
+
+    private func hideSearchButton() {
+        // Improvement suggestion: #01
+        // Here is a simple solution, made just for now, which looks to be working for current layout and
+        // which could be broken if more buttons in navBar will be needed to add in the future...
+        // In real case it would be a suggestion for Designer to move "Search" button to the "unitsAvailable" screen's contentView
+        // and avoid placing it in NavBar.
+        // With such improvement it could appear together with content view, and that will improve general UX impresion to the User.
+        if navigationItem.rightBarButtonItems?.count == 2 {
+            navigationItem.rightBarButtonItems?.removeLast()
         }
     }
 
@@ -190,7 +210,7 @@ extension ControlUnits.ViewController: UISearchResultsUpdating {
 
 extension ControlUnits.ViewController: UISearchBarDelegate {
 
-    public func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
         navigationItem.searchController = nil
     }
 
