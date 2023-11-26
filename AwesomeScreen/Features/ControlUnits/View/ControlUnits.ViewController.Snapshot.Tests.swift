29149e3a37a72f306a1f83f970a920dcf0171883
TonyJem <a.jemanov@gmail.com>
2023-11-26 18:54:32 +0200

    Remove snapshotesting package from app

--- AwesomeScreen/Features/ControlUnits/View/ControlUnits.ViewController.Snapshot.Tests.swift
+++ AwesomeScreen/Features/ControlUnits/View/ControlUnits.ViewController.Snapshot.Tests.swift
@@ -1,17 +1,33 @@
 import XCTest
-import SnapshotTesting
+// import SnapshotTesting
 @testable import AwesomeScreen
 
 // swiftlint: disable all
 
 final class ControlUnitsViewControllerSnapshotTests: XCTestCase {
 
-    var presenter: ControlUnits.Presenter!
-    var interactorMock: String!
-    var viewController: ControlUnits.ViewController!
-
-    override func setUp() {
+//    var interactorMock: ControlUnitsInteractorMock!
+//    var presenter: ControlUnits.Presenter!
+//    var viewController: ControlUnits.ViewController!
+//
+//    override func setUp() {
 //        isRecording = true
+//    }
+
+    func testControlUnitsEmptyView() {
+
+//        interactorMock = ControlUnitsInteractorMock()
+//
+//        presenter = ControlUnits.Presenter(
+//            interactor: interactorMock,
+//            cacheService: CacheService()
+//        )
+//
+//        presenter.viewDidAppear()
+//
+//        viewController = ControlUnits.ViewController(with: <#T##ControlUnits.Presenter#>)
+//        verifyViewController(viewController)
+
     }
 
 }
