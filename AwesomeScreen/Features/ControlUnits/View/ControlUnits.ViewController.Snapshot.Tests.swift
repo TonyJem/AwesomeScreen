3d73a238092c8e432c851bcdedd31587d934a27b
TonyJem <a.jemanov@gmail.com>
2023-11-26 18:20:58 +0200

    Create ControlUnitsViewControllerSnapshotTests template

---
+++ AwesomeScreen/Features/ControlUnits/View/ControlUnits.ViewController.Snapshot.Tests.swift
@@ -0,0 +1,19 @@
+import XCTest
+import SnapshotTesting
+@testable import AwesomeScreen
+
+// swiftlint: disable all
+
+final class ControlUnitsViewControllerSnapshotTests: XCTestCase {
+
+    var presenter: ControlUnits.Presenter!
+    var interactorMock: String!
+    var viewController: ControlUnits.ViewController!
+
+    override func setUp() {
+//        isRecording = true
+    }
+
+}
+
+// swiftlint: enable all
---
+++ AwesomeScreenTests/Extensions/XCTestCase+VCSnapshot.swift
@@ -0,0 +1,42 @@
+import XCTest
+import SnapshotTesting
+import SwiftUI
+
+extension XCTestCase {
+
+    func verifyViewController(
+        _ viewController: UIViewController,
+        file: StaticString = #filePath,
+        line: UInt = #line,
+        testName: String = #function,
+        record recording: Bool = false
+    ) {
+
+        let devices: [String: ViewImageConfig] = [
+            "iPhoneSe": .iPhoneSe,
+            "iPhone8": .iPhone8,
+            "iPhoneX": .iPhoneX,
+            "iPhoneXsMax": .iPhoneXsMax,
+        ]
+
+        let results = devices.map { device in
+            verifySnapshot(
+                matching: viewController,
+                as: .image(
+                    on: device.value,
+                    precision: 0.99,
+                    perceptualPrecision: 0.99
+                ),
+                named: "\(device.key)",
+                record: recording,
+                file: file,
+                testName: testName,
+                line: line
+            )
+        }
+
+        results.forEach { XCTAssertNil($0, file: file, line: line) }
+
+    }
+
+}
