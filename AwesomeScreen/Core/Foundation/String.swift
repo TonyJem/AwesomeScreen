863393643eadbb22eced6bf309686d2aff7c6ef8
TonyJem <a.jemanov@gmail.com>
2023-11-26 08:53:53 +0200

    005: Minor changes in project file-tree

--- AwesomeScreen/Core/Foundation/String.swift
+++ AwesomeScreen/Core/Foundation/String.swift
@@ -1,8 +1,5 @@
 import Foundation
 
-// TODO: Need to look at whole project later and decide if I should to add "public var", "public class" and etc.
-// with intention to show that it is ready to be expandable (if into few modules it will be vissible everywhere)
-
 extension String {
 
     /// Returns the localized `self`.
