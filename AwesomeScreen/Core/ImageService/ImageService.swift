251c98e0fc1193f37c5cc244fcff581fcbaa79f3
TonyJem <a.jemanov@gmail.com>
2023-11-26 07:28:07 +0200

    005: Remove old ImageLoaderService

--- AwesomeScreen/Core/ImageService/ImageService.swift
+++ AwesomeScreen/Core/ImageService/ImageService.swift
@@ -1,6 +1,31 @@
 import UIKit
 import SwiftUI
 
+// TODO: Delete it when is not needed
+/*
+ import SwiftUI
+
+ final class ImageLoaderService: ObservableObject {
+     @Published var image: UIImage = UIImage()
+
+     func loadImage(for urlString: String) {
+         guard let url = URL(string: urlString) else { return }
+
+         // TODO: Decide if we should handle "response"
+         // TODO: Decide if we should handle "error"
+
+         let task = URLSession.shared.dataTask(with: url) { data, _, _ in
+             guard let data = data else { return }
+             DispatchQueue.main.async {
+                 self.image = UIImage(data: data) ?? .awesomeImage(.noImage)
+             }
+         }
+         task.resume()
+     }
+
+ }
+ */
+
 // TODO: Remove old ImageLoaderService
 // TODO: Think if we need 2 download methods here or is enought to have just one
 final class ImageService: ObservableObject {
